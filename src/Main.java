344044cb1a50bf0ce6b2d0c7c48558d86347c59f
Ram Sathwik Tirumalasetti <ramsathwik@myyahoo.com>
2025-03-15 11:06:44 +0530

    making sure all files are uploaded.

--- src/Main.java
+++ src/Main.java
@@ -5,66 +5,7 @@ public class Main {
 
     Scanner s = new Scanner(System.in);
 
-    ArrayList<Task> lists = new ArrayList<Task>();
-
-    public void addtask(String name){
-        int i = 0;
-        Task curtask = new Task(name);
-        lists.add(curtask);
-
-//        System.out.println("Tasks: ");
-//        for (Task t : lists)
-//        {
-//            System.out.println(t.id +" "+ t.name);
-//        }
-    }
-
-    public void display(){
-
-        if (lists.isEmpty())
-        {
-            System.out.println("No tasks currently. ");
-        }
-
-        for (Task t : lists)
-        {
-            System.out.println(t.id + ". " + t.name);
-        }
-    }
-
-    public void delete() {
-        System.out.println("Select tasks to delete: ");
-        String[] deleteid = s.nextLine().split(" ");
-        s.nextLine();
-    }
-
     public static void main(String[] args) {
-        int id = 0;
-        Main app = new Main();
-        app.addtask("Hello there");
-        app.addtask("Hi");
-        app.display();
 
-
-        System.out.println("Click x or y to add or delete tasks. Click z to exit. ");
-        String choicestr = app.s.next();
-        char choice = choicestr.charAt(0);
-        app.s.nextLine();
-//        int x = app.s.nextInt();
-//        app.s.nextLine();
-
-        if (choice == 'x') {
-            System.out.println("Enter: ");
-            String str = app.s.nextLine();
-            app.addtask(str);
-            System.out.println("Tasks: ");
-        for (Task t : app.lists)
-        {
-            System.out.println(t.id +" "+ t.name);
-        }
-        }
-
-        else if (choice == 'y') {
-        }
-    }
+        Tasklist app = new Tasklist();
 }
---
+++ src/Tasklist.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tasklist {
+
+    ArrayList<Task> lists = new ArrayList<Task>();
+    Tasklist() {
+
+    }
+
+    public void addtask(String name){
+        int i = 0;
+        Task curtask = new Task(name);
+        lists.add(curtask);
+    }
+
+    public void display(){
+
+        if (lists.isEmpty())
+        {
+            System.out.println("No tasks currently. ");
+        }
+
+        for (Task t : lists)
+        {
+            System.out.println(t.id + ". " + t.name);
+        }
+    }
+
+}
